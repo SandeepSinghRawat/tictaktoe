077329dca19cb7b580e6121bbf2be97dc6583ef8
SandeepSinghRawat <rawatsandeep521993@gmail.com>
2024-06-06 21:05:28 +0530

    ssh test

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
           <Player name="Player 1" symbol="X" />
           <Player name="Player 2" symbol="O" />
         </ol>
-        Game Board
+
       </div>
     </main>
   );
